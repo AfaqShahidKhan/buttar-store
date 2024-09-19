9f9ec1a06ed4dd9eac5390cf09776c9b0dae910c
AfaqShahidKhan <afaqk3203@gmail.com>
2024-09-20 01:28:34 +0500

    modify directory style

---
+++ src/app/sign_in/page.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+export const metadata = {
+    title: "Sign Up",
+  };
+function page() {
+  return <div>Sign In page</div>;
+}
+
+export default page;
---
+++ src/app/sign_up/page.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+export const metadata = {
+  title: "SignIn",
+};
+function page() {
+  return <div>Sign up page</div>;
+}
+
+export default page;
