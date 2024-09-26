b0df05ee836c51993e7cfa6bd772d859592afcf2
AfaqShahidKhan <afaqk3203@gmail.com>
2024-09-27 00:51:56 +0500

    fix shopcategory

--- src/components/ShopCategory.jsx
+++ src/components/ShopCategory.jsx
@@ -1,24 +1,8 @@
-import React, { useContext } from 'react'
-import dropdown_icon from 
+import React from 'react'
 
-const ShopCategory = (props) => {
-    const {all_product} = useContext(ShopContext);
+function ShopCategory() {
   return (
-    <div className='shop-category'>
-        <img src={props.banner} />
-        <div className='shopcategory-indexSort'>
-            <p>
-                <span>showing 1-12</span> out of 36 products
-            </p>
-            <div className='shopcategory-sort'>
-                sort by <img src=" " />
-
-
-            </div>
-
-        </div>
-      
-    </div>
+    <div>ShopCategory</div>
   )
 }
 
